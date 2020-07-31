218612f449d4d303df238eb458638cf01ea9a7bc
tomoaki <tomenbluecips@gmail.com>
2020-07-31 15:28:46 +0900

    test: :white_check_mark: test base setting

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -13,13 +13,15 @@
 #  created_at   :datetime         not null
 #  updated_at   :datetime         not null
 #
-require 'rails_helper'
-
-RSpec.describe Book, type: :model do
-  let(:book) { create(:book) }
-
-  it '有効ファクトリーの確認' do
-    expect(book).to be_valid
+FactoryBot.define do
+  factory :book do
+    # --TODO association設定--
+    name { 'マシューのゆめ' }
+    image { File.new("#{Rails.root}/spec/factories/images/test.png") }
+    publisher { '好学社' }
+    author_name { 'レオ＝レオニ' }
+    author_image { File.new("#{Rails.root}/spec/factories/images/test2.png") }
+    price { 600 }
+    isbn { 4-7690-2017-1 }
   end
-
 end
