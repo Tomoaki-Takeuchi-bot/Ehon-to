329cedbe9fd515226237139d0b944050b9b8571b
tomoaki <tomenbluecips@gmail.com>
2020-08-06 18:36:02 +0900

    style: :pencil2: indent revision

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,11 +13,11 @@ class CommentsController < ApplicationController
 
   private
 
-  def set_book
-    @book = Book.find_with_comments(params[:book_id])
-  end
+    def set_book
+      @book = Book.find_with_comments(params[:book_id])
+    end
 
-  def comment_params
-    params.require(:comment).permit(:comment).merge(user_id: current_user.id)
-  end
+    def comment_params
+      params.require(:comment).permit(:comment).merge(user_id: current_user.id)
+    end
 end
