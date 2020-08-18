cd251ece034be5e648fb6782eafc33be483d9e83
tomoaki <tomenbluecips@gmail.com>
2020-08-18 09:33:03 +0900

    feat: :sparkles: image-update-function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[name, image, image_cache])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name image image_cache])
   end
 end
