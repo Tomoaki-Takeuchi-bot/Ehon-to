b914d22cb26c73aa3bdbe0990c46c22893b70cd5
tomoaki <tomenbluecips@gmail.com>
2020-08-29 22:43:51 +0900

    feat: :sparkles: password reset by mail function

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,27 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # --追加事項 gmail設定--
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  if Rails.application.credentials.gmail.present?
+    mail_address = Rails.application.credentials.gmail[:address]
+    password = Rails.application.credentials.gmail[:password]
+  else
+    mail_address = 'admin@example.com'
+    password = 'password'
+  end
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    enable_starttls_auto: true,
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: mail_address,
+    password: password,
+    authentication: 'plain'
+  }
 
   config.action_mailer.perform_caching = false
 
