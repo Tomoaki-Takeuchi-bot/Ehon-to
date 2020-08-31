fa4cfad83b2b8bdebb87f980eadc88252fd686fd
tomoaki <tomenbluecips@gmail.com>
2020-08-31 15:18:52 +0900

    chore: :construction_worker: avator size change ( 40 to 100 )

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,9 +18,7 @@
 #  index_users_on_email                 (email) UNIQUE
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+class User < ApplicationRecord # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable # Include default devise modules. Others available are:
   devise :database_authenticatable,
          :registerable,
          :recoverable,
@@ -29,7 +27,9 @@ class User < ApplicationRecord
 
   attr_accessor :current_password
 
+  THUMBNAIL_SIZE = [100, 100]
   mount_uploader :image, ImageUploader
+
   validates :name, presence: true, length: { maximum: 30 }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
@@ -39,25 +39,22 @@ class User < ApplicationRecord
             presence: true,
             uniqueness: true,
             length: { maximum: 255 },
-            format: {
-              with: VALID_EMAIL_REGEX
-            }
+            format: { with: VALID_EMAIL_REGEX }
 
   has_many :books, dependent: :destroy
   has_many :comments, dependent: :destroy
-  has_many :favorites, dependent: :destroy
-  # タグ機能（Userモデルからいいねした本を取得するためリレーション追加）
-  has_many :favorite_books, through: :favorites, source: :book
-  #---
-  # ユーザーフォロー機能---
+  has_many :favorites, dependent: :destroy # タグ機能（Userモデルからいいねした本を取得するためリレーション追加）
+  has_many :favorite_books, through: :favorites, source: :book # ユーザーフォロー機能---
   has_many :relationships
   has_many :followings, through: :relationships, source: :follow
-  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
-  has_many :followers, through: :reverse_of_relationships, source: :user
-  #---
+  has_many :reverse_of_relationships,
+           class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverse_of_relationships, source: :user #---
 
   def follow(other_user_id)
-    relationships.find_or_create_by(follow_id: other_user_id) unless id == other_user_id.to_i
+    unless id == other_user_id.to_i
+      relationships.find_or_create_by(follow_id: other_user_id)
+    end
   end
 
   def unfollow(other_user_id)
