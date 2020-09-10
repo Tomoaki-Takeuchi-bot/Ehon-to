faf25378eebbed12217c59a70ff6e0fcccec1e9b
tomoaki <tomenbluecips@gmail.com>
2020-09-10 16:30:18 +0900

    build: :building_construction: seeds image link change to s3

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,12 @@ Faker::Config.locale = :ja
 
 1.upto(20) do |i|
   name = Faker::Name.name
-  image = open("#{Rails.root}/db/fixtures/avatar/avatar-#{i}.jpg")
+  remote_image_url =
+    "http://image.ehon-to.net/DB/fixutures/avatar/avatar-#{i}.jpg"
   email = "sample-#{i}@example.com"
   password = 'password'
   User.create!(
-    image: image,
+    remote_image_url: remote_image_url,
     name: name,
     email: email,
     password: password,
@@ -18,7 +19,7 @@ end
 User.find_by(email: 'sample-1@example.com').books.create!(
   tag_list: %w[こころがあったかい話],
   name: 'こんとあき',
-  image: open("#{Rails.root}/db/fixtures/ehon/ehon-1.jpg"),
+  remote_image_url: 'http://image.ehon-to.net/DB/fixutures/ehon/ehon-1.jpg',
   publisher: '福音館書店',
   author_name: '林明子',
   price: 1300,
@@ -28,7 +29,7 @@ User.find_by(email: 'sample-1@example.com').books.create!(
 User.find_by(email: 'sample-2@example.com').books.create!(
   tag_list: %w[ゆかいな話],
   name: 'きんぎょがにげた',
-  image: open("#{Rails.root}/db/fixtures/ehon/ehon-2.jpg"),
+  remote_image_url: 'http://image.ehon-to.net/DB/fixutures/ehon/ehon-2.jpg',
   publisher: '福音館書店',
   author_name: '五味太郎',
   price: 900,
@@ -38,7 +39,7 @@ User.find_by(email: 'sample-2@example.com').books.create!(
 User.find_by(email: 'sample-3@example.com').books.create!(
   tag_list: %w[ゆかいな話],
   name: 'おばけの天ぷら',
-  image: open("#{Rails.root}/db/fixtures/ehon/ehon-3.jpg"),
+  remote_image_url: 'http://image.ehon-to.net/DB/fixutures/ehon/ehon-3.jpg',
   publisher: 'ポプラ社',
   author_name: 'せなけいこ',
   price: 1200,
@@ -48,7 +49,7 @@ User.find_by(email: 'sample-3@example.com').books.create!(
 User.find_by(email: 'sample-4@example.com').books.create!(
   tag_list: %w[ゆかいな話 こころがあったかい話],
   name: 'どろんこハリー',
-  image: open("#{Rails.root}/db/fixtures/ehon/ehon-4.jpg"),
+  remote_image_url: 'http://image.ehon-to.net/DB/fixutures/ehon/ehon-4.jpg',
   publisher: '福音館書店',
   author_name: 'マーガレット・ブロイ・グレアム',
   price: 1200,
@@ -58,7 +59,7 @@ User.find_by(email: 'sample-4@example.com').books.create!(
 User.find_by(email: 'sample-5@example.com').books.create!(
   tag_list: %w[ないた話 こころがあったかい話],
   name: 'ずーっとずっとだいすきだよ',
-  image: open("#{Rails.root}/db/fixtures/ehon/ehon-5.jpg"),
+  remote_image_url: 'http://image.ehon-to.net/DB/fixutures/ehon/ehon-5.jpg',
   publisher: '評論社',
   author_name: 'ハンス・ウィルヘルム',
   price: 1200,
@@ -68,7 +69,7 @@ User.find_by(email: 'sample-5@example.com').books.create!(
 User.find_by(email: 'sample-6@example.com').books.create!(
   tag_list: %w[こころがあったかい話],
   name: 'おしいれのぼうけん',
-  image: open("#{Rails.root}/db/fixtures/ehon/ehon-6.jpg"),
+  remote_image_url: 'http://image.ehon-to.net/DB/fixutures/ehon/ehon-6.jpg',
   publisher: '童心社',
   author_name: 'ふるたたるひ',
   price: 1300,
@@ -78,7 +79,7 @@ User.find_by(email: 'sample-6@example.com').books.create!(
 User.find_by(email: 'sample-7@example.com').books.create!(
   tag_list: %w[ためになる話],
   name: 'スイミー',
-  image: open("#{Rails.root}/db/fixtures/ehon/ehon-7.jpg"),
+  remote_image_url: 'http://image.ehon-to.net/DB/fixutures/ehon/ehon-7.jpg',
   publisher: '好学社',
   author_name: 'レオ・レオニ',
   price: 1456,
@@ -88,7 +89,7 @@ User.find_by(email: 'sample-7@example.com').books.create!(
 User.find_by(email: 'sample-8@example.com').books.create!(
   tag_list: %w[ふしぎな話],
   name: 'かようびのよる',
-  image: open("#{Rails.root}/db/fixtures/ehon/ehon-8.jpg"),
+  remote_image_url: 'http://image.ehon-to.net/DB/fixutures/ehon/ehon-8.jpg',
   publisher: '徳間書店',
   author_name: 'デヴィット・ウィーズナー',
   price: 1400,
@@ -98,7 +99,7 @@ User.find_by(email: 'sample-8@example.com').books.create!(
 User.find_by(email: 'sample-9@example.com').books.create!(
   tag_list: %w[ためになる話],
   name: 'おこだでませんように',
-  image: open("#{Rails.root}/db/fixtures/ehon/ehon-9.jpg"),
+  remote_image_url: 'http://image.ehon-to.net/DB/fixutures/ehon/ehon-9.jpg',
   publisher: '小学館',
   author_name: 'くすのきしげのり',
   price: 1500,
@@ -108,7 +109,7 @@ User.find_by(email: 'sample-9@example.com').books.create!(
 User.find_by(email: 'sample-10@example.com').books.create!(
   tag_list: %w[こころがあったかい話],
   name: 'マシューのゆめ',
-  image: open("#{Rails.root}/db/fixtures/ehon/ehon-10.jpg"),
+  remote_image_url: 'http://image.ehon-to.net/DB/fixutures/ehon/ehon-10.jpg',
   publisher: '好学社',
   author_name: 'レオ・レオニ',
   price: 1456,
