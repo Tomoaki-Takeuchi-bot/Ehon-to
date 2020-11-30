c8f8d60c76ebf74eb0e914615b3758993ac2e8e8
tomoaki <tomenbluecips@gmail.com>
2020-12-01 08:37:07 +0900

    chore: :construction_worker: add errors (application controller)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,40 @@ class ApplicationController < ActionController::Base
     books_path
   end
 
+  # 例外ハンドル
+  unless Rails.env.development?
+    rescue_from Exception,                        with: :_render_500
+    rescue_from ActiveRecord::RecordNotFound,     with: :_render_404
+    rescue_from ActionController::RoutingError,   with: :_render_404
+  end
+
+  def routing_error
+    raise ActionController::RoutingError, params[:path]
+  end
+
+  private
+
+  def _render_404(e = nil)
+    logger.info "Rendering 404 with exception: #{e.message}" if e
+
+    if request.format.to_sym == :json
+      render json: { error: '404 error' }, status: :not_found
+    else
+      render 'errors/404', status: :not_found
+    end
+  end
+
+  def _render_500(e = nil)
+    logger.error "Rendering 500 with exception: #{e.message}" if e
+    Airbrake.notify(e) if e # Airbrake/Errbitを使う場合はこちら
+
+    if request.format.to_sym == :json
+      render json: { error: '500 error' }, status: :internal_server_error
+    else
+      render 'errors/500', status: :internal_server_error
+    end
+  end
+
   # protectedで同じクラスやサブクラスの中であれば呼び出し可能にしている
   protected
 
